255145323eb09add253824dbeac211e2e5ecdd11
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-10-05 19:05:44 +0000

    goroutines

--- main.go
+++ main.go
@@ -2,29 +2,17 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"time"
 )
 
-func main() {
-	// A slice of sample websites
-	urls := []string{
-		"https://www.easyjet.com/",
-		"https://www.skyscanner.de/",
-		"https://www.ryanair.com",
-		"https://wizzair.com/",
-		"https://www.swiss.com/",
-	}
-	for _, url := range urls {
-		checkUrl(url)
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(1000 * time.Millisecond)
+		fmt.Println(s)
 	}
 }
 
-//checks and prints a message if a website is up or down
-func checkUrl(url string) {
-	_, err := http.Get(url)
-	if err != nil {
-		fmt.Println(url, "is down !!!")
-		return
-	}
-	fmt.Println(url, "is up and running.")
+func main() {
+	go say("world")
+	say("hello")
 }
