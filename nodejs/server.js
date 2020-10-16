e2d12cfec252599035540300a27c7f9a9d12e659
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-10-16 11:46:26 +0000

    #3 Fix js styles

--- nodejs/server.js
+++ nodejs/server.js
@@ -4,13 +4,13 @@ const http = require('http');
 const products = require('./data/products.json');
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/api/products' && req.method === 'GET') {
-        res.writeHead(200, {'Content-Type': 'application/json'})
-        res.end(JSON.stringify(products))   
-    } else {
-        res.writeHead(200, {'Content-Type': 'application/json'})
-        res.end(JSON.stringify({message: 'Route not found' })) 
-    }
+  if (req.url === '/api/products' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(products));
+  } else {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Route not found' }));
+  }
 });
 
 const PORT = process.env.PORT || 5000;
