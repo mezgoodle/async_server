addcdfd268e12875c282f95a3b44652bd4189790
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-10-20 17:00:20 +0300

    Create utils.js

---
+++ nodejs/utils.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const fs = require('fs');
+const { stringify } = require('querystring');
+
+const writeDataToFile = (filename, content) => {
+  fs.writeFileSync(filename, JSON,stringify(content), 'utf8', (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max))
+
+module.exports = {
+  writeDataToFile,
+  getRandomInt,
+}
