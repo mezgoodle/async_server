75b0de3631d9b7d48ffc552afa644ade78e3e4b0
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-10-21 16:52:19 +0000

    #3 Fix problem

--- nodejs/controllers/productController.js
+++ nodejs/controllers/productController.js
@@ -70,10 +70,10 @@ async function updateProduct(req, res, id) {
     } else {
       const body = await getPostData(req);
 
-      const { title, description, price } = JSON.parse(body);
+      const { name, description, price } = JSON.parse(body);
 
       const productData = {
-        title: title || product.title,
+        name: name || product.name,
         description: description || product.description,
         price: price || product.price,
       };
