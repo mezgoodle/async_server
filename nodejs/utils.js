9b71f610a4aea30fe8037397a74ced040ad445f9
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-10-21 10:36:58 +0300

    #3 Create new function in utils.js

--- nodejs/utils.js
+++ nodejs/utils.js
@@ -11,9 +11,28 @@ const writeDataToFile = (filename, content) => {
   });
 };
 
+const getPostData = (req) => {
+  return new Promise((resolve, reject) => {
+    try {
+      let body = '';
+
+      req.on('data', (chunk) => {
+        body += chunk.toString();
+      })
+
+      req.on('end', () => {
+        resolve(body)
+      })
+    } catch (error) {
+      reject(err);
+    }
+  })
+};
+
 const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
 
 module.exports = {
   writeDataToFile,
   getRandomInt,
+  getPostData,
 };
