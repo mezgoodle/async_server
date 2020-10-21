4c6c57cb8d0199a69ed13df12b048afd18ed2307
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-10-21 16:22:33 +0000

    Some updates

--- nodejs/server.js
+++ nodejs/server.js
@@ -9,7 +9,7 @@ const server = http.createServer((req, res) => {
   } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET') {
     const id = req.url.split('/')[3];
     getProduct(req, res, id);
-  } else if (req.url === 'api/products' && req.method === 'POST') {
+  } else if (req.url === '/api/products' && req.method === 'POST') {
     createProduct(req, res);
   } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT') {
     const id = req.url.split('/')[3];
