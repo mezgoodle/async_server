9ede477f1a4ef13493ea14e8a84ffa3792f063e5
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-10-21 10:44:24 +0300

    #3 Create controller for the update route

--- nodejs/controllers/productController.js
+++ nodejs/controllers/productController.js
@@ -58,8 +58,39 @@ async function createProduct(req, res) {
   }
 }
 
+// @desc Update A Product
+// @route PUT /api/product/:id
+async function updateProduct(req, res, id) {
+  try {
+    const product = await Product.findById(id);
+
+    if (!product) {
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'Product Not Found' }));
+    } else {
+      const body = await getPostData(req);
+
+      const { title, description, price } = JSON.parse(body);
+
+      const productData = {
+        title: title || product.title,
+        description: description || product.description,
+        price: price || product.price,
+      };
+
+      const updProduct = await Product.update(id, productData);
+
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      return res.end(JSON.stringify(updProduct));
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 module.exports = {
   getProducts,
   getProduct,
   createProduct,
+  updateProduct,
 };
