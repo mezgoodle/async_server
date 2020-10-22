0b5128dd2daa79d08d680c0311d6ba80530a2436
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-10-22 09:23:43 +0300

    #3 Update server by adding os fork method

--- python/server.py
+++ python/server.py
@@ -1,24 +1,38 @@
+import os
 import socket
 import sys
 import time
 
-
 def run_server(port=53210):
     serv_sock = create_serv_sock(port)
+    active_children = set()
     cid = 0
     while True:
         client_sock = accept_client_conn(serv_sock, cid)
-        serve_client(client_sock, cid)
+        child_pid = serve_client(client_sock, cid)
+        active_children.add(child_pid)
+        reap_children(active_children)
         cid += 1
 
 def serve_client(client_sock, cid):
+    child_pid = os.fork()
+    if child_pid:
+        client_sock.close()
+        return child_pid
+
     request = read_request(client_sock)
     if request is None:
         print(f'Client #{cid} unexpectedly disconnected')
     else:
         response = handle_request(request)
         write_response(client_sock, response, cid)
+    os._exit(0)
 
+def reap_children(active_children):
+    for child_pid in active_children.copy():
+        child_pid, _ = os.waitpid(child_pid, os.WNOHANG)
+        if child_pid:
+            active_children.discard(child_pid)
 
 def create_serv_sock(serv_port):
     serv_sock = socket.socket(socket.AF_INET,
@@ -31,7 +45,7 @@ def create_serv_sock(serv_port):
 def accept_client_conn(serv_sock, cid):
     client_sock, client_addr = serv_sock.accept()
     print(f'Client #{cid} connected '
-          f'{client_addr[0]}:{client_addr[1]}')
+        f'{client_addr[0]}:{client_addr[1]}')
     return client_sock
 
 def read_request(client_sock, delimiter=b'!'):
