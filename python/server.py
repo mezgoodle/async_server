c4cdefe0787671c962e931a4fefa493d0c702064
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-10-23 21:10:26 +0300

    #4 Change project. install FastAPI

--- python/server.py
+++ python/server.py
@@ -1,24 +1,2 @@
-from aiohttp.web import Application, run_app
-
-from controller import RestResource
-from models import Note
-from sqlalchemy import engine_from_config
-
-
-notes = {}
-app = Application()
-person_resource = RestResource(
-    'notes',
-    Note,
-    notes,
-    ('title',
-     'description',
-     'created_at',
-     'created_by',
-     'priority'),
-    'title')
-person_resource.register(app.router)
-
-
-if __name__ == '__main__':
-    run_app(app)
+from fastapi import FastAPI
+from pydantic import BaseModel
