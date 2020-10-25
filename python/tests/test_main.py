d0c90b3a333a8e584c3e69c85ff5e87d52340664
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-10-25 11:00:41 +0200

    #4 Add some tests

--- python/tests/test_main.py
+++ python/tests/test_main.py
@@ -10,3 +10,38 @@ async def test_root():
         response = await ac.get('/')
     assert response.status_code == 200
     assert response.json() == {'msg' : 'Hello world!'}
+
+
+@pytest.mark.asyncio
+async def test_failure():
+    async with AsyncClient(app=app, base_url='http://test') as ac:
+        response = await ac.get('/city')
+    assert response.status_code == 200
+    assert response.json() == {"detail":"Not Found"}
+
+
+@pytest.mark.asyncio
+async def test_cities():
+    async with AsyncClient(app=app, base_url='http://test') as ac:
+        response = await ac.get('/cities')
+    assert response.status_code == 200
+    assert response.json() == [
+  {
+    "id": 3,
+    "name": "Adak",
+    "timezone": "America/Adak",
+    "current_time": "2020-10-25T00:00:16.304182-09:00"
+  },
+  {
+    "id": 4,
+    "name": "Yellowknife",
+    "timezone": "America/Yellowknife",
+    "current_time": "2020-10-25T03:00:16.352230-06:00"
+  },
+  {
+    "id": 5,
+    "name": "Amsterdam",
+    "timezone": "Europe/Amsterdam",
+    "current_time": "2020-10-25T10:00:16.352080+01:00"
+  }
+]
